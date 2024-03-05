53dab3cc857da0dee1bfd1f88c2cb73e031b9056
C0DE-Seb <sebastian.sanchez25@ycdsbk12.ca>
2024-03-05 10:18:38 -0500

    Done quiz game with feedback changes

--- quiz.py
+++ quiz.py
@@ -5,52 +5,55 @@ Date: 2024/03/04
 Description: Multiple choice quiz with final score
 """
 
-print("-------------------------------")
-print("Multiple-Choice Quiz Game")
+print("------------------------------- \
+      \nMultiple-Choice Quiz Game")
 
 #Initialize score
 score = 0
 
 #First question
-print("\n1. What is the power house of the cell?")
-print("(a) Nucleus")
-print("(b) Mitochondria")
-print("(c) Cell Membrane")
+print("\n1. What is the power house of the cell? \
+      \n(a) Nucleus \
+      \n(b) Mitochondria \
+      \n(c) Cell Membrane \
+      ")
 
-first_answer = input("\n> ")
+answer = input("\n> ")
 
 #Is the first users answer right
-if first_answer == "b":
+if answer == "b":
     print("Correct!")
     score = score + 1
 else:
     print("Incorrect.")
 
 #Second question
-print("\n2. Who discovered gravity")
-print("(a) Isaac Newton")
-print("(b) Albert Einstein")
-print("(c) Charles Darwin")
+print("\n2. Who discovered gravity \
+      \n(a) Isaac Newton \
+      \n(b) Albert Einstein \
+      \n(c) Charles Darwin \
+      ")
 
-second_answer = input("\n> ")
+answer = input("\n> ")
 
 #Is the second users answer right
-if second_answer == "a":
+if answer == "a":
     print("Correct!")
     score = score + 1
 else:
     print("Incorrect.")
 
 #Third question
-print("\n3. Which element is a non-metal")
-print("(a) Iron")
-print("(b) Sodium")
-print("(c) Oxygen")
+print("\n3. Which element is a non-metal \
+      \n(a) Iron \
+      \n(b) Sodium \
+      \n(c) Oxygen \
+      ")
 
-third_answer = input("\n> ")
+answer = input("\n> ")
 
 #Is the third users answer right
-if third_answer == "c":
+if answer == "c":
     print("Correct!")
     score = score + 1
 else:
@@ -60,6 +63,6 @@ else:
 final_score = score / 3 * 100
 
 #Final results
-print("\nQuiz complete!")
-print("You answered " + str(score) + " out of 3 questions correctly. Your score is " + str(round(final_score, 1)) + "%")
-print("-------------------------------")
+print("\nQuiz complete! \
+      \nYou answered " + str(score) + " out of 3 questions correctly. Your score is " + str(round(final_score, 1)) + "% \
+      \n-------------------------------")
